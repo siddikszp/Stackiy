5fa2958ab77e0ff68f8fc4ab5dbc3b2e95c59617
siddikszp <siddikszp@gmail.com>
2025-11-09 23:59:33 +0600

    New Branch

---
+++ Algomid.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int a,b,piss;
+   a=10;
+   b=30;
+   piss = a + b;
+
+   cout<<"Show result: "<<piss<<endl;
+   return 0;
+}
+
+
+
+
+
+
+
+
